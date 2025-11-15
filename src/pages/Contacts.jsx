679cf80f3e31bd352a1e240692cc27225831329a
Askhat Taskovich <qq.tynaev@gmail.com>
2025-11-16 00:37:24 +0600

    Update Contacts.jsx

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -14,7 +14,7 @@ function Contacts() {
         setIsLoading(true);
         setError(null);
         const response = await fetch(
-          "https://github.com/Taskov1ch-Repos/trash/raw/refs/heads/main/data/contacts.json"
+          "https://raw.githubusercontent.com/Taskov1ch-Repos/trash/refs/heads/main/data/contacts.json"
         );
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
