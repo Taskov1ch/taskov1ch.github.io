94eccf8db9fbf8bf002241f8189bf5503be6579a
Askhat Taskovich <qq.tynaev@gmail.com>
2025-11-16 00:38:05 +0600

    Update Donate.jsx

--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -14,7 +14,7 @@ function Donate() {
         setIsLoading(true);
         setError(null);
         const response = await fetch(
-          "https://github.com/Taskov1ch-Repos/trash/raw/refs/heads/main/data/donations.json"
+          "https://raw.githubusercontent.com/Taskov1ch-Repos/trash/refs/heads/main/data/donations.json"
         );
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
